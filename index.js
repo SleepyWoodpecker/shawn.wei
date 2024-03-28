780a09dc365119296c5b4c21ee2ea30f6a1c0e64
SleepyWoodpecker <shawnweichew@gmail.com>
2024-03-28 22:36:50 +0800

    finished up section 2, starting on section 3

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const experienceTab = document.querySelector("#experience-tab");
 const experienceSection = document.querySelector("#experience-section");
 
-experienceTab.addEventListener("click", () => {
-  experienceSection.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-    inline: "center",
+const personalProjectsTab = document.querySelector("#personal-projects-tab");
+const personalProjectsSection = document.querySelector(
+  "#personal-projects-section",
+);
+
+const technologiesTab = document.querySelector("#technologies-tab");
+const technologiesSection = document.querySelector("#technologies-section");
+
+const tabSectionPairs = [
+  { tab: experienceTab, section: experienceSection },
+  { tab: personalProjectsTab, section: personalProjectsSection },
+  { tab: technologiesTab, section: technologiesSection },
+];
+
+for (const { tab, section } of tabSectionPairs) {
+  tab.addEventListener("click", () => {
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "center",
+    });
   });
-});
+}
