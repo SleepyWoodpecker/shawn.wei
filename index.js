e28b2372f111afbe6985af3be96a51b11e6ba13b
SleepyWoodpecker <shawnweichew@gmail.com>
2024-03-29 11:05:17 +0800

    heh not too bad I think

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// scrolling
 const experienceTab = document.querySelector("#experience-tab");
 const experienceSection = document.querySelector("#experience-section");
 
@@ -23,4 +24,78 @@ for (const { tab, section } of tabSectionPairs) {
       inline: "center",
     });
   });
+}
+
+// technologies images (I did this here because I think it would be too repetitive to write out the html for each one)
+const backend = document.querySelector("#backend-logos");
+const devToolsAndMore = document.querySelector("#dev-tools-and-more-logos");
+const frontend = document.querySelector("#frontend-logos");
+
+const images = [
+  {
+    parentElement: backend,
+    logos: [
+      {
+        link: "fastapi.svg",
+        name: "FastAPI",
+      },
+      {
+        link: "flask.svg",
+        name: "Flask",
+      },
+      {
+        link: "postgresql.svg",
+        name: "PostgreSQL",
+      },
+      {
+        link: "nodejs.svg",
+        name: "Node",
+      },
+    ],
+  },
+  {
+    parentElement: devToolsAndMore,
+    logos: [
+      { link: "docker.svg", name: "Docker" },
+      { link: "git.svg", name: "Git" },
+      { link: "pytorch.svg", name: "Pytorch" },
+      { link: "playwright.svg", name: "Playwright" },
+    ],
+  },
+  {
+    parentElement: frontend,
+    logos: [
+      { link: "react.svg", name: "React" },
+      { link: "tailwind.svg", name: "Tailwind" },
+      { link: "html.svg", name: "HTML" },
+      { link: "css.svg", name: "CSS" },
+    ],
+  },
+];
+
+for (const { parentElement, logos } of images) {
+  for (const { link, name } of logos) {
+    const wrappingDiv = document.createElement("div");
+    wrappingDiv.classList.add(
+      "flex",
+      "flex-col",
+      "jusitfy-center",
+      "items-center",
+      "gap-3",
+    );
+
+    const logoDisplay = document.createElement("img");
+    logoDisplay.src = `images/${link}`;
+    logoDisplay.alt = name;
+    logoDisplay.classList.add("h-16", "w-16");
+    logoDisplay.loading = "lazy";
+
+    const description = document.createElement("p");
+    description.innerText = name;
+
+    wrappingDiv.appendChild(logoDisplay);
+    wrappingDiv.appendChild(description);
+
+    parentElement.appendChild(wrappingDiv);
+  }
 }
